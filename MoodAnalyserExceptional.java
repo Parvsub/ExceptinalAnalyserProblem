0ee24cce39cabe585a02158d2e8be645cfcec6c8
Parvsub <Praveenboda421@gmail.com>
2023-03-05 13:07:21 +0530

    Added: Create Constuctor

---
+++ MoodAnalyserExceptional.java
@@ -0,0 +1,25 @@
+package exceptionalAssignment;
+
+public class MoodAnalyserExceptional extends Exception {
+    public exceptionType type;
+
+    public MoodAnalyserExceptional(exceptionType type, String message){
+        super(message);
+        this.type = type;
+    }
+
+    public MoodAnalyserExceptional(exceptionType type, Throwable cause) {
+        super(cause);
+        this.type = type;
+    }
+
+    public MoodAnalyserExceptional(exceptionType type,String message, Throwable cause){
+        super(message);
+        new MoodAnalyserExceptional(type,cause);
+    }
+
+    public enum exceptionType {
+        ENTERED_EMPTY, ENTERED_NULL, NO_SUCH_CLASS, NO_SUCH_METHOD, METHOD_INVOCATION_ISSUE
+    }
+}
+
