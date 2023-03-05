2c48a7327dba016ee12c44273da033b69a121189
Parvsub <Praveenboda421@gmail.com>
2023-03-05 12:06:37 +0530

    Added:Method to pass the Testcase

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -1,16 +1,21 @@
 package exceptionalAssignment;
 
 public class MoodAnalyser {
-        private String message;
-        public MoodAnalyser(String message) {
-            this.message = message;
-        }
+    private String message;
 
-        public String analyseMood() {
-            if(message.contains("Sad"))
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
+        try {
+            if (message.contains("Sad"))
                 return "SAD";
             else
                 return "HAPPY";
+        } catch (NullPointerException e) {
+            return "HAPPY";
         }
     }
+}
 
--- Welcome.java
+++
@@ -1,7 +0,0 @@
-package exceptionalAssignment;
-
-public class Welcome {
-    public static void main(String[] args) {
-        System.out.println("Welcome");
-    }
-}
